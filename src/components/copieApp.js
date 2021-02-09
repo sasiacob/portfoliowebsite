bc13b2c050c2235a0da3f0947f376db15fb2b954
Sas Iacob <sasiacobconstantin@yahoo.com>
2021-02-09 23:09:30 +0200

    first commit

--- src/components/copieApp.js
+++ src/components/copieApp.js
@@ -1,47 +1,27 @@
 function App() {
-    return (
-      
-  
-      <div className="App">
-  
-        <div className="App-Navbar">
-          
-        
-          <h1>Portfolio</h1>
-          
-          <nav>
-            <ul className="nav__links">
-              <li><a href="/">Home</a></li>
-              <li><a href="/mywork">My work</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
-  
-         
-            </ul>
-          </nav>
-         
-        </div>
-        <header className="App-header">
-        {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-       
-      <div className="App-content">
-        <h1 className="content-element first-element"> Sas Iacob Constantin Homepage </h1>
-          <p className="content-element">
-            <b>Hello world and welcome to my homepage!</b>
-                    </p>
-          <p className="content-element">
-          My name is Sas Iacob Constantin, and this is my website. You can take a look at my projects, key informations about me, or contact me.
-  
-  
-          </p>
-          <p>Currently I'm a Computer Science student, on the 4th year.</p>
-         
-          </div>
-        </header>
-  
-     
-      </div>
-    );
-  }
-  
-  export default App;
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/*  <img src={logo} className="App-logo" alt="logo" />*/}
+
+				<div className="App-content">
+					<h1 className="content-element first-element">
+						{" "}
+						Sas Iacob Constantin Homepage{" "}
+					</h1>
+					<p className="content-element">
+						<b>Hello world and welcome to my homepage!</b>
+					</p>
+					<p className="content-element">
+						My name is Sas Iacob Constantin, and this is my website. You can
+						take a look at my projects, key informations about me, or contact
+						me.
+					</p>
+					<p>Currently I'm a Computer Science student, on the 4th year.</p>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+export default App;
